80b131e4ebd526f11aae832dad7faa72b6a5210e
gladyswambura <gladyswahito7@gmail.com>
2022-06-05 10:20:16 +0300

    creates a profile update and user update view

--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,176 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import Http404,HttpResponseRedirect
+from django.urls import reverse, resolve
+from .forms import UserRegisterForm
+from django.contrib import messages
+from . forms import UpdateProfileForm, UpdateUserForm, NewPostform, NewCommentForm
+from django.contrib.auth.decorators import login_required
+from .models import Post, Profile, Follow, Stream, Likes, Comment
+from django.http import JsonResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.db.models import Q
 
-# Create your views here.
+# AUTHENTICATION--(Signup)
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get['email']
+            messages.success(request, f'Hurray your account was created!!')
+
+            # Automatically Log In The User
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'],)
+            login(request, new_user)
+            # return redirect('editprofile')
+            return redirect('index')
+
+    elif request.user.is_authenticated:
+        return redirect('login')
+    else:
+        form = UserRegisterForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'sign-up.html', context)
+
+
+# MAIN PAGE VIEW
+def index(request):
+    user = request.user
+    user = request.user
+    all_users = User.objects.all()
+    # follow_status = Follow.get_or_create.filter(following=user, follower=request.user).exists()
+
+    profile = Profile.objects.all()
+
+    posts = Stream.objects.filter(user=user)
+    group_ids = []
+
+    
+    for post in posts:
+        group_ids.append(post.post_id)
+        
+    post_items = Post.objects.filter(id__in=group_ids).all().order_by('-posted')
+
+    query = request.GET.get('q')
+    if query:
+        users = User.objects.filter(Q(username__icontains=query))
+
+        page_number = request.GET.get('page')
+
+    context = {
+        'post_items': post_items,
+        'profile': profile,
+        'all_users': all_users,
+    }
+    return render(request, 'gram/index.html', context)
+
+# NEW POST FUNCTION
+@login_required
+def NewPost(request):
+    user = request.user
+    profile = get_object_or_404(Profile, user=user)
+    tags_obj = []
+    
+    if request.method == "POST":
+        form = NewPostform(request.POST, request.FILES)
+        if form.is_valid():
+            picture = form.cleaned_data.get('picture')
+            caption = form.cleaned_data.get('caption')
+            tag_form = form.cleaned_data.get('tags')
+            tag_list = list(tag_form.split(','))
+
+            for tag in tag_list:
+                t, created = tag.objects.get_or_create(title=tag)
+                tags_obj.append(t)
+            p, created = Post.objects.get_or_create(picture=picture, caption=caption, user=user)
+            p.tags.set(tags_obj)
+            p.save()
+            return redirect('profile', request.user.username)
+    else:
+        form = NewPostform()
+    context = {
+        'form': form
+    }
+    return render(request, 'newpost.html', context)
+
+# POST-DETAILS FUNCTION
+@login_required
+def PostDetail(request, post_id):
+    user = request.user
+    post = get_object_or_404(Post, id=post_id)
+    comments = Comment.objects.filter(post=post).order_by('-date')
+
+    if request.method == "POST":
+        form = NewCommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.user = user
+            comment.save()
+            return HttpResponseRedirect(reverse('post-details', args=[post.id]))
+    else:
+        form = NewCommentForm()
+
+    context = {
+        'post': post,
+        'form': form,
+        'comments': comments
+    }
+
+    return render(request, 'postdetail.html', context)
+
+
+# LIKE POST FUNCTION
+@login_required
+def like(request, post_id):
+    user = request.user
+    post = Post.objects.get(id=post_id)
+    current_likes = post.likes
+    liked = Likes.objects.filter(user=user, post=post).count()
+
+    if not liked:
+        Likes.objects.create(user=user, post=post)
+        current_likes = current_likes + 1
+    else:
+        Likes.objects.filter(user=user, post=post).delete()
+        current_likes = current_likes - 1
+        
+    post.likes = current_likes
+    post.save()
+    # return HttpResponseRedirect(reverse('post-details', args=[post_id]))
+    return HttpResponseRedirect(reverse('post-details', args=[post_id]))
+
+# FAVOURITE POST FUNCTION
+@login_required
+def favourite(request, post_id):
+    user = request.user
+    post = Post.objects.get(id=post_id)
+    profile = Profile.objects.get(user=user)
+
+    if profile.favourite.filter(id=post_id).exists():
+        profile.favourite.remove(post)
+    else:
+        profile.favourite.add(post)
+    return HttpResponseRedirect(reverse('post-details', args=[post_id]))
+
+
+# **********Profile view starts here******************
+# PROFILE FUNCTION
+@login_required
+def profile(request):
+    userupdate = UpdateUserForm()
+    profileupdate = UpdateProfileForm()
+
+    context = {
+        'userupdate': userupdate,
+        'profileupdate': profileupdate
+    }
+    return render(request, 'users/profile.html', context)
+
+
+
