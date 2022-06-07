0db67ded4f92ee2f1e9ead440a2de1f994544051
gladyswambura <gladyswahito7@gmail.com>
2022-06-07 09:29:45 +0300

    creates the first test

--- gram/tests.py
+++ gram/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from .models import Comment, Profile, Image
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.user = User(username='hezron')
+        self.user.save()
+        self.profile = Profile(id=12 ,profile='image.jpg', bio='Test profile',
+                                    user=self.user)
