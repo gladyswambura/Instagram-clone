ebe3de4a6f486394b93c9082593ab506514eda83
gladyswambura <gladyswahito7@gmail.com>
2022-06-07 08:26:34 +0300

    fix: changes the path of comments

---
+++ gram/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, re_path
+from . import views
+from .views import index, NewPost, PostDetail,like, favourite
+from . import views as app_views
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # PROFILE SECTION
+    path('profile/',views.profile,name='profile'),
+    path('profile/update/',app_views.update_profile,name='update_profile'),
+    # MAIN PAGE && POST SECTION
+    path('', views.index, name='index'),
+    path('newpost', app_views.NewPost, name='newpost'),
+    path('post/<int:pk>/', views.PostDetail, name='post-detail'),
+    re_path(r'^like/(?P<operation>.+)/(?P<pk>\d+)/',views.like, name='like'),
+    path('comment/<int:pk>/', views.add_comment, name='comment'),
+    # re_path(r'^comment/(?P<pk>\d+)',views.add_comment,name='comment'),
+    
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
