cc73eb33d73ed881a575b2ca14123dbe14ff2382
gladyswambura <gladyswahito7@gmail.com>
2022-06-08 08:05:29 +0300

    runs tests

--- gram/tests.py
+++ gram/tests.py
@@ -9,3 +9,15 @@ class ProfileTestCase(TestCase):
         self.user.save()
         self.profile = Profile(id=12 ,profile='image.jpg', bio='Test profile',
                                     user=self.user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+    def test_save_profile(self):
+        self.profile.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) >0)
+    def test_update_profile(self):
+        self.profile.save_profile()
+        self.profile.update_profile(self.profile.user_id)
+        self.profile.save_profile()
+        self.assertTrue(Profile,self.profile.user)
